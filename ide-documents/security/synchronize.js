120e22eb24be732a1826614f54e5f6f169477b57
ThuF <jordan.p.pavlov@gmail.com>
2020-06-04 22:09:03 +0300

    fixes

--- ide-documents/security/synchronize.js
+++ ide-documents/security/synchronize.js
@@ -1,6 +1,6 @@
-var utils = require("ide-documents/security/accessUtils");
+var accessUtils = require("ide-documents/security/accessUtils");
 
-let accessDefinitions = utils.getAccessDefinitions();
-utils.updateAccessDefinitions(accessDefinitions);
+let accessDefinitions = accessUtils.getAccessDefinitions();
+accessUtils.updateAccessDefinitions(accessDefinitions);
 
 console.log("Access Definitions successfully synchronized by [ide-documents-security] Job");
