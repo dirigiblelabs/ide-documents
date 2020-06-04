ec55b46bb0dcdf5ce6da7fc03205036a7278b5b6
ThuF <jordan.p.pavlov@gmail.com>
2020-06-04 17:53:45 +0300

    Save access constraints into CMIS

--- ide-documents/services/access.js
+++ ide-documents/services/access.js
@@ -11,6 +11,16 @@
 var rs = require("http/v4/rs");
 var repositoryContent = require("repository/v4/content");
 var repositoryManager = require("repository/v4/manager");
+var documentsApi = require("ide-documents/api/lib/document");
+var folderApi = require("ide-documents/api/lib/folder");
+var cmisObjectApi = require("ide-documents/api/lib/object");
+var bytes = require("io/v4/bytes");
+var streams = require("io/v4/streams");
+
+const INTERNAL_FOLDER = "__internal";
+const INTERNAL_FOLDER_LOCATION = "/" + INTERNAL_FOLDER;
+const ACCESS_CONSTRAINTS_FILE = "roles-access.json";
+const ACCESS_CONSTRAINTS_FILE_LOCATION = "/" + INTERNAL_FOLDER + "/" + ACCESS_CONSTRAINTS_FILE;
 
 rs.service()
     .resource("")
@@ -36,7 +46,20 @@ rs.service()
 .execute();
 
 function getAccessDefinitions() {
-    return JSON.parse(repositoryContent.getText("ide-documents/security/roles.access"));
+    let document = documentsApi.getDocument(ACCESS_CONSTRAINTS_FILE_LOCATION);
+    let content = {
+        constraints: []
+    };
+
+    try {
+        let inputStream = documentsApi.getDocumentStream(document);
+        let data = inputStream.getStream().readBytes();
+        content = JSON.parse(bytes.byteArrayToText(data));
+    } catch (e) {
+        // Do nothing
+    }
+    // let content = JSON.parse(repositoryContent.getText("ide-documents/security/roles.access"));
+    return content;
 }
 
 function updateAccessDefinitions(accessDefinitions) {
@@ -47,4 +70,30 @@ function updateAccessDefinitions(accessDefinitions) {
     	repositoryManager.deleteResource(path);
     }
     repositoryManager.createResource(path, content);
+    updateAccessDefinitionsInCMS(content);
+}
+
+function updateAccessDefinitionsInCMS(data) {
+    let folder = folderApi.getFolder(INTERNAL_FOLDER_LOCATION);
+    if (!folder) {
+        let rootFolder = folderApi.getFolderOrRoot("/");
+        folderApi.createFolder(rootFolder, INTERNAL_FOLDER);
+        folder = folderApi.getFolder(INTERNAL_FOLDER_LOCATION);
+    }
+    let document = {
+        getName: function() {
+            return ACCESS_CONSTRAINTS_FILE;
+        },
+        getContentType: function() {
+            return "application/json";
+        },
+        getSize: function() {
+            return data.length;
+        },
+        getInputStream: function() {
+            let contentBytes = bytes.textToByteArray(data);
+            return streams.createByteArrayInputStream(contentBytes);
+        }
+    }
+    documentsApi.uploadDocument(folder, document);
 }
